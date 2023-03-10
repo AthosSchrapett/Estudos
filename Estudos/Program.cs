916328d84aade87beb628c6620aefba2ee5e817b
Athos Schrapett <athosschrapett@outlook.com>
2023-03-10 11:00:24 -0300

    feat: initial create

---
+++ Estudos/Program.cs
@@ -0,0 +1,15 @@
+﻿using Estudos.Util;
+using Spire.Xls;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        //var lista = ReadFile.Read();
+        //ReadFile.Write(lista);
+
+        Workbook wb = new Workbook();
+        wb.LoadFromFile(@"D:\Meus Projetos\Estudos\Estudos gerais\Estudos\Excel\Formulário Logs.xlsx");
+        wb.SaveToFile(@"D:\Meus Projetos\Estudos\Estudos gerais\Estudos\Excel\output.xlsb", ExcelVersion.Xlsb2010);
+    }
+}
