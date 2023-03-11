9e1c5e47e0f08e1387c7568a3b4260a609f484d6
Athos Schrapett <athosschrapett@outlook.com>
2023-03-11 10:07:21 -0300

    feat: create delegate

---
+++ Delegates/Delegates/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudos.Delegates
+{
+    public class Message
+    {
+        public delegate string ObterMensagemEspecifica();
+
+        public static string ObterMensagem(ObterMensagemEspecifica mensagemEspecifica)
+        {
+            string mensagem = $"essa mensagem retornou do delegate: { mensagemEspecifica() } !!!";
+
+            return mensagem;
+        }
+    }
+}
